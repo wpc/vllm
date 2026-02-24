218dce41c89e990d2380259a4e390185b017d4e9
Pengchao Wang <wpc@fb.com>
2026-02-24 13:12:20 -0800

    [offload connector][bugfix] skip kv_cache_stride_order reshape

--- vllm/v1/kv_offload/worker/cpu_gpu.py
+++ vllm/v1/kv_offload/worker/cpu_gpu.py
@@ -259,16 +259,11 @@ class CpuGpuOffloadingHandlers:
                 assert gpu_shape[0] == 2
                 split_k_and_v = True
 
-            try:
-                kv_cache_stride_order = attn_backend.get_kv_cache_stride_order(
-                    include_num_layers_dimension=has_layers_dim
-                )
-                assert len(kv_cache_stride_order) == len(gpu_shape)
-            except (AttributeError, NotImplementedError):
-                kv_cache_stride_order = tuple(range(len(gpu_shape)))
-
-            # permute test_shape according to stride_order
-            test_shape = tuple(test_shape[i] for i in kv_cache_stride_order)
+            # NOTE: gpu_tensor.shape is in LOGICAL order (the tensor was
+            # permuted back via inv_order in _reshape_kv_cache_tensors),
+            # so we should NOT apply kv_cache_stride_order to test_shape.
+            # The test_shape from get_kv_cache_shape is already in logical
+            # order, matching gpu_tensor.shape.
 
             # find block_size (16) dimension index
             block_size_idx = test_shape.index(16)
